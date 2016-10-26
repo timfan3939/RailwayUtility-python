8b84e9a5746631d56efa0557433108fe679ce93e
C.T.Fan <C.T.Fan>
2016-10-26 20:31:46 +0800

    added some trains, but train\'s schedule has not converted to station\'s schedule

--- RUTimeTable.py
+++ RUTimeTable.py
@@ -58,10 +58,10 @@ class RUTTStationNode:
 class RUTTTrainNode:
 	def __init__ (self, id):
 		self.id = id
-		schedule = []
-		operate_days = ''
-		departure_station = None
-		destination_station = None
+		self.schedule = []
+		self.operate_days = ''
+		self.departure_station = None
+		self.destination_station = None
 		
 	def __str__ (self):
 		return str.format( 'RUTTTrainNode\n-\\{0}', self.id )
@@ -119,6 +119,19 @@ def main():
 	timetable.line_list.append(line1)
 	timetable.line_list.append(line2)
 	
+	# Add Trains 10x to Line 1
+	# Test case Train added first
+	for i in range(1, 10, 2):
+		train = RUTTTrainNode( str.format('10{}', i) )
+		for j in range( len(line1.line) ):
+			if j != 0:
+				train.schedule.append( RUTTNode( line1.line[j], train,  300+30*i+3*j, RUTTNodeType.RUTTArrival ) )
+			if j != len(line1.line):
+				train.schedule.append( RUTTNode( line1.line[j], train, 300+30*i+3*j+1, RUTTNodeType.RUTTDeparture ) )
+		timetable.all_train_list.append(train)
+	
+	# TODO: transfer train's schedule to station's schedule
+	
 	
 	print('All Stations')
 	for i in timetable.all_station_list:
@@ -129,6 +142,14 @@ def main():
 		print(i.name)
 		for j in i.line:
 			print(' ', j.station.name, j.milestone)
+	
+	print('-'*20)
+	print('All Train')
+	for i in timetable.all_train_list:
+		print('Train No.', i.id)
+		for j in i.schedule:
+			print ( j.station.station.name, j.time_stamp )
+		print()
 	pass
 	
 
