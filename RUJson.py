31e38958ca383fa4b63a1968fc5882c171a3ceb7
Tim Fan 范植添 <timfan@micb2b.com>
2017-02-17 17:07:38 +0800

    Add loading json file and the structure of the json file.

--- RUJson.py
+++ RUJson.py
@@ -3,8 +3,34 @@
 import json
 import RUTimeTable
 
+# TrainInfos
+# |- Type
+# |- Train
+# |- BreastFeed
+# |- Route
+# |- Package
+# |- OverNightStn
+# |- LineDir
+# |- Line
+# |- Dinning
+# |- Cripple
+# |- CarClass
+# |- Bike
+# |- Note
+# |- NoteEng
+# \- TimeInfos
+#    |- Route
+#    |- Station
+#    |- Order
+#    |- DepTime
+#    |- ArrTime
+
 def main():
-	pass
+	with open('file/20161106.json', encoding='utf8') as inputJson:
+		data = json.load(inputJson)
+	
+	for trainInfo in data['TrainInfos']:
+		print(trainInfo['Train'], trainInfo['Note'])
 
 if __name__ == '__main__':
 	main()
