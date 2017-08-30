00da810e1bc6137faaaa6dd87184212e157127d1
Tim Fan 范植添 <timfan@micb2b.com>
2017-08-30 15:49:07 +0800

    Add a viewer to timetable

---
+++ RUTTViewer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+class RUTTViewer:
+	pass
+	
+	
+
+	
+	
+	
+	
+	
+	
+####################
+# Test Function
+####################
+# Create a viewer with Timetable
+# Read Station
+# Read Time Table
+# Show interactive view of RUTimeTable
+
+
+def main():
+	pass
+
+	
+if __name__ == '__main__':
+	main()
